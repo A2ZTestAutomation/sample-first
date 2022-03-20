623be1f78eb6bfc3b16161bc4c0c4ca7b47af55b
A2ZTestAutomation <corpdevops@gmail.com>
2022-03-20 16:54:44 +0530

    Modified .index file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -23,17 +23,17 @@
 //   // `config` is the resolved Cypress config
 // }
 
-const fs = require('fs-extra')
-const path = require('path')
-function getConfigurationByFile(file){
-  const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
-  return fs.readJson(pathToConfigFile)
-}
-module.exports = (on, config) => {
-    const file = config.env.configFile || 'dev'
-    return getConfigurationByFile(file)
+// const fs = require('fs-extra')
+// const path = require('path')
+// function getConfigurationByFile(file){
+//   const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+//   return fs.readJson(pathToConfigFile)
+// }
+// module.exports = (on, config) => {
+//     const file = config.env.configFile || 'dev'
+//     return getConfigurationByFile(file)
 
-  }
+//   }
 
   module.exports = (on, config) => {
     on('task', {
